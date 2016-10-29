6684b597c952e861b15d4ae283bc4e6b77186186
Johannes Elgaard <joha4270@gmail.com>
2016-10-29 14:48:57 +0200

    Something works in options, dont remember what anymore. But compiles and needs to upload...

---
+++ Chummer/Backend/Attributes/OptionDisplayAttributes/DisplayConfigurationAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Chummer.Backend.Attributes.OptionDisplayAttributes
+{
+    internal sealed class DisplayConfigurationAttribute : System.Attribute
+    {
+        public DisplayConfigurationAttribute(string description, string tooltip = "")
+        {
+            try
+            {
+                DisplayName = LanguageManager.Instance.GetString(description);
+                if (!string.IsNullOrWhiteSpace(tooltip))
+                {
+
+                    Tooltip = LanguageManager.Instance.GetString(tooltip);
+                }
+            }
+            catch (Exception)
+            {
+                DisplayName = $"[{description}]";
+                Tooltip = $"[{tooltip}]";
+                Log.Error($"Failure to find string key either {description} {tooltip}");
+            }
+
+        }
+
+        public string DisplayName { get; set; }
+        public string Tooltip { get; set; }
+    }
+}
